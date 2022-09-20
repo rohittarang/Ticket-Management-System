9f4638cb1b210ec0e3ab3125a5a33168b455afc6
rohittarang <85814325+rohittarang@users.noreply.github.com>
2022-09-20 23:23:14 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('',views.Index,name='index'),
-    path('delete/<int:id>/',views.Delete,name='delete'),
-    path('edit/<int:id>/',views.Edit,name='edit'),
-]
